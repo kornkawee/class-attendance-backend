adb4cd0a1a6df63d6ac2d5174b71f9f74bae2705
kornkawee <kornkawee.non11@gmail.com>
2025-09-25 13:03:43 +0700

    add env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,16 +13,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MetadataModule } from './metadata/metadata.module';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot({
+    TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'node53939-belusysdb-1.th1.proen.cloud',
-      port: 11320,  
-      username: 'quizdev_mm',
-      password: 'Ot[TxPQ6]zrf4JKU',
-      database: 'quizdev_mm',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306'),  
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: [Student, Classroom, Prefix, Gender, Gradelevel, StudentClassroom],
       synchronize: false, // DB มี schema แล้ว -> false
       charset: 'utf8mb4',
